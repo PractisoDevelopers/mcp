aa09d206be481427bf02ceffe6bfc88a3833ab17
zhufucdev <zhufuzhufu1@gmail.com>
2025-12-07 11:08:57 +0800

    :memo: correct help format

--- practiso_mcp/__main__.py
+++ practiso_mcp/__main__.py
@@ -3,6 +3,6 @@ from argparse import ArgumentParser
 from practiso_mcp import main
 
 parser = ArgumentParser()
-parser.add_argument("--transport", default="stdio", help="Communication method this server uses.", choices=['stdio', 'streamable-http', 'sse'])
+parser.add_argument("--transport", default="stdio", help="communication method this server uses", choices=['stdio', 'streamable-http', 'sse'])
 args = parser.parse_args()
 main(args.transport)
